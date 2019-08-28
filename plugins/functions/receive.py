ea0b8a5037cff363b1c9c59db5bf59e2e29e0c03
Xiao <xiao@scp-079.org>
2019-08-28 07:59:04 +0000

    - 稍加修改

--- plugins/functions/receive.py
+++ plugins/functions/receive.py
@@ -103,7 +103,7 @@ def receive_config_reply(client: Client, data: dict) -> bool:
         link = data["config_link"]
         text = (f"管理员：{user_mention(uid)}\n"
                 f"操作：{code('更改设置')}\n"
-                f"说明：{code('请点击下方按钮进行设置')}")
+                f"说明：{code('请点击下方按钮进行设置')}\n")
         markup = InlineKeyboardMarkup(
             [
                 [
