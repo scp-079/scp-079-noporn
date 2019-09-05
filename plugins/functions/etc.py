5b4059da4123b50e4721debc2565e4f71e9c5277
Xiao <xiao@scp-079.org>
2019-09-05 07:20:19 +0000

    - 稍加修改

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -237,7 +237,7 @@ def get_full_name(user: User) -> str:
     return text
 
 
-def get_int(text: str) -> int:
+def get_int(text: str) -> Optional[int]:
     # Get a int from a string
     result = None
     try:
