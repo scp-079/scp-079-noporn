a41a7a755ca3332bdf2e2e6c20206f22f2fd2d74
Xiao <xiao@scp-079.org>
2019-09-06 07:52:50 +0000

    - 0.2.9

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -264,7 +264,9 @@ except_ids: Dict[str, Set[str]] = {
 user_ids: Dict[int, Dict[str, Dict[Union[int, str], Union[float, int]]]] = {}
 # user_ids = {
 #     12345678: {
-#         "detected": {},
+#         "detected": {
+#               -10012345678: 1512345678
+#         },
 #         "score": {
 #             "captcha": 0.0,
 #             "clean": 0.0,
