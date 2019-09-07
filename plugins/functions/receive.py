b0d47197f780de1016ca69046020056fbfbb0d00
Xiao <xiao@scp-079.org>
2019-09-07 12:59:00 +0000

    - 0.3.0

--- plugins/functions/receive.py
+++ plugins/functions/receive.py
@@ -187,9 +187,9 @@ def receive_preview(client: Client, message: Message, data: dict) -> bool:
     # Receive message's preview
     try:
         gid = data["group_id"]
+        uid = data["user_id"]
+        mid = data["message_id"]
         if glovar.admin_ids.get(gid):
-            uid = data["user_id"]
-            mid = data["message_id"]
             preview = receive_file_data(client, message, True)
             if preview:
                 image = preview["image"]
