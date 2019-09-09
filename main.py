6b5cdcfcd17bd5749c07bb1fddd86dbb7da6eba2
Xiao <xiao@scp-079.org>
2019-09-09 02:27:29 +0000

    - 0.3.0

--- main.py
+++ main.py
@@ -31,12 +31,11 @@ from plugins.functions.timers import update_admins, update_status
 # Enable logging
 logger = logging.getLogger(__name__)
 
-# Start
+# Config session
 app = Client(
     session_name="bot",
     bot_token=glovar.bot_token
 )
-app.start()
 
 # Timer
 scheduler = BackgroundScheduler()
