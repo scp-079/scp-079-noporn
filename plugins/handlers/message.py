6e1580e9da5d161396978107175be4a52714ada3
Xiao <xiao@scp-079.org>
2019-09-16 02:46:09 +0000

    - 0.3.0

--- plugins/handlers/message.py
+++ plugins/handlers/message.py
@@ -22,7 +22,7 @@ from pyrogram import Client, Filters, Message
 
 from .. import glovar
 from ..functions.channel import get_debug_text
-from ..functions.etc import code, get_text, thread, user_mention
+from ..functions.etc import code, general_link, get_text, thread, user_mention
 from ..functions.file import save
 from ..functions.filters import class_c, class_d, class_e, declared_message, exchange_channel, from_user, hide_channel
 from ..functions.filters import is_ban_text, is_declared_message, is_nsfw_media, is_nsfw_url, is_restricted_channel
@@ -83,7 +83,7 @@ def check(client: Client, message: Message) -> bool:
 
 @Client.on_message(Filters.incoming & Filters.channel & hide_channel
                    & ~Filters.command(glovar.all_commands, glovar.prefix), group=-1)
-def exchange_emergency(_: Client, message: Message) -> bool:
+def exchange_emergency(client: Client, message: Message) -> bool:
     # Sent emergency channel transfer request
     try:
         # Read basic information
@@ -101,6 +101,11 @@ def exchange_emergency(_: Client, message: Message) -> bool:
                             glovar.should_hide = data
                         elif data is False and sender == "MANAGE":
                             glovar.should_hide = data
+
+                        text = (f"项目编号：{general_link(glovar.project_name, glovar.project_link)}\n"
+                                f"执行操作：{code('频道转移')}\n"
+                                f"应急频道：{code((lambda x: '启用' if x else '禁用')(glovar.should_hide))}\n")
+                        thread(send_message, (client, glovar.debug_channel_id, text))
 
         return True
     except Exception as e:
