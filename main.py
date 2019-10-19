e7f1de9b784ebb15d90faba1a0939d51c3fb06a1
Xiao <xiao@scp-079.org>
2019-10-19 05:15:25 +0000

    - 0.3.6

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ app.start()
 update_status(app, "online")
 
 # Timer
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(job_defaults={"misfire_grace_time": 60})
 scheduler.add_job(interval_min_10, "interval", minutes=10)
 scheduler.add_job(update_status, "cron", [app, "awake"], minute=30)
 scheduler.add_job(backup_files, "cron", [app], hour=20)
